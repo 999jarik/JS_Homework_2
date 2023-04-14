d9248bdefc350860fb7fc6eecd91cbbe6c27c029
Yaroslav Trofimenko <92098465+999jarik@users.noreply.github.com>
2023-04-14 23:17:09 +0300

    Summary

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let bodyParent = document.body
+let title = document.createElement('h1');
+    title.className = ("title");
+    title.innerHTML = "Тест по програмуванню";
+let wrapper = document.createElement('div');
+    wrapper.className = ("wrapper");
+
+
+
+let genPage = {
+
+};
